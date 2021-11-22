8d5e3152902605838bcdb45a7fc80e4e72e0a01e
hc_PC <1069043992@qq.com>
2021-11-22 23:15:56 +0800

    通过编译

--- src/tiger/translate/translate.h
+++ src/tiger/translate/translate.h
@@ -37,6 +37,12 @@ public:
 class ProgTr {
 public:
   // TODO: Put your lab5 code here */
+  ProgTr(std::unique_ptr<absyn::AbsynTree> absyn_tree,
+         std::unique_ptr<err::ErrorMsg> erromsg)
+      : absyn_tree_(std::move(absyn_tree)), errormsg_(std::move(erromsg)),
+
+        tenv_(std::make_unique<env::TEnv>()),
+        venv_(std::make_unique<env::VEnv>()) {}
 
   /**
    * Translate IR tree
@@ -50,7 +56,6 @@ public:
   std::unique_ptr<err::ErrorMsg> TransferErrormsg() {
     return std::move(errormsg_);
   }
-
 
 private:
   std::unique_ptr<absyn::AbsynTree> absyn_tree_;
